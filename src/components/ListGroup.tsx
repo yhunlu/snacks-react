b54dfbf56278f2caa3336524bd62bd2190bb7934
Yahya Unlu <yhunlu87@gmail.com>
2023-11-26 20:21:49 +0300

    handling events, selectedIndex.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,48 +1,37 @@
+import { useState } from 'react';
 
 const ListGroup = () => {
-//   only city names of america
-  let items = [
+  //   only city names of america
+  const items = [
     'New York',
     'San Francisco',
     'Los Angeles',
     'Chicago',
-    'Houston',
-    'Phoenix',
-    'Philadelphia',
-    'San Diego',
-    'Dallas',
-    'San Antonio',
-    'Jacksonville',
-    'Fort Worth',
-    'Seattle',
-    'Denver',
-    'El Paso',
-    'Washington',
-    'Boston',
-    'Memphis',
-    'Detroit',
-    'Nashville',
-    'Milwaukee',
-    'Portland',
-    'Oklahoma City',
-    'Las Vegas',
-  ]
+  ];
 
-  items = []
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item} className="list-group-item">
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className={
+              selectedIndex === index
+                ? 'list-group-item active'
+                : 'list-group-item'
+            }
+            onClick={() => setSelectedIndex(index)}
+          >
             {item}
           </li>
         ))}
       </ul>
     </>
-  )
-}
+  );
+};
 
-export default ListGroup
+export default ListGroup;
