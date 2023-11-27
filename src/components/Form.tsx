5925d72478ceb110ddef3324ce5b271724d7a2a6
Yahya Unlu <yhunlu87@gmail.com>
2023-11-27 21:02:43 +0300

    react hook form with typed validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,16 @@
 import { FieldValues, useForm } from 'react-hook-form';
 
+interface FormData {
+  name: string;
+  age: number;
+}
+
 const Form = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   const onSubmit = (data: FieldValues) => {
     console.log(data);
@@ -14,11 +23,17 @@ const Form = () => {
           Name
         </label>
         <input
-          { ...register('name')}
+          {...register('name', { required: true, minLength: 3 })}
           id="name"
           type="text"
           className="form-control"
         />
+        {errors.name?.type === 'required' && (
+          <p className="text-danger">Name is required</p>
+        )}
+        {errors.name?.type === 'minLength' && (
+          <p className="text-danger">Name must be at least 3 characters</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
@@ -30,6 +45,7 @@ const Form = () => {
           type="number"
           className="form-control"
         />
+        {errors.age && <p className="text-danger">Age is required</p>}
       </div>
       {/* create button */}
       <button type="submit" className="btn btn-primary">
